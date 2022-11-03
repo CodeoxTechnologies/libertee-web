acc791326157be541a25e71e736bbc674befa639
shabeeb <shabeebkaip@gmail.com>
2022-11-03 20:00:06 +0530

    worked on mobile responsive

--- src/Products/index.js
+++ src/Products/index.js
@@ -1,7 +1,7 @@
 import waterProof from '../assets/images/water.png';
 import soundWave from '../assets/images/sound-wave.png';
 import medical from '../assets/images/medical.png';
-import prodImage1 from '../assets/images/Tracys-Dog-22.jpg';
+import prodImage1 from '../assets/images/Tracys-Dog-small-23.jpeg';
 import prodImage2 from '../assets/images/le-wand-massager-18.png';
 import prodImage3 from '../assets/images/28db68fe-69eb-43cb-bb30-ca9fe6ce3662._CR0,0,1200,1200_PT0_SX220__-30.jpg';
 export const productsList = [
@@ -16,7 +16,7 @@ export const productsList = [
     {
         name: "Rabbit Vibrator",
         price: 2899,
-        image: prodImage2,
+        image:  prodImage3,
         color: [{ color: "#FF005C", active: true }, { color: "#5F008", active: false }, { color: "#0060D1", active: false }],
         activeColor: "#FF005C",
         features: [{ name: "Waterproof", image: waterProof }, { name: "5 patterns 5 intensities", image: soundWave, }, { name: "medical grade silicone", image: medical }],
@@ -24,7 +24,7 @@ export const productsList = [
     {
         name: "Rabbit Vibrator",
         price: 2899,
-        image: prodImage3,
+        image: prodImage2,
         color: [{ color: "#FF005C", active: true }, { color: "#5F008", active: false }, { color: "#0060D1", active: false }],
         activeColor: "#FF005C",
         features: [{ name: "Waterproof", image: waterProof }, { name: "5 patterns 5 intensities", image: soundWave, }, { name: "medical grade silicone", image: medical }],
