ab2e2db70156b017671931a0831d1db7b2ad456a
shabeeb <shabeebkaip@gmail.com>
2022-12-05 13:31:49 +0530

    cart ui

--- src/Payment/components/CartTotal.js
+++ src/Payment/components/CartTotal.js
@@ -4,8 +4,7 @@ import { Grid } from "@material-ui/core";
 const CartTotal = () => {
   return (
     <div className="cart-total">
-      <div className="coupon-container"></div>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} style={{ margin: "10px" }}>
         <Grid item md={7} lg={7}>
           <div className="coupon-input-container">
             <input
@@ -18,6 +17,52 @@ const CartTotal = () => {
         <Grid item md={5} lg={5}>
           <div className="coupon-button-container">
             <button className="coupon-button">Apply</button>
+          </div>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} style={{ margin: "10px" }}>
+        <Grid item md={7} lg={7}>
+          <div className="cart-subtotal-container">
+            <div className="cart-subtotal-heading">Sub Total:</div>
+          </div>
+        </Grid>
+        <Grid item md={5} lg={5}>
+          <div className="cart-subtotal-price-container">
+            <div className="cart-subtotal-price">Rs 9999/-</div>
+          </div>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} style={{ margin: "10px" }}>
+        <Grid item md={7} lg={7}>
+          <div className="cart-subtotal-container">
+            <div className="cart-subtotal-heading">Shipping:</div>
+          </div>
+        </Grid>
+        <Grid item md={5} lg={5}>
+          <div className="cart-subtotal-price-container">
+            <div className="cart-subtotal-price">Rs 40/-</div>
+          </div>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} style={{ margin: "10px" }}>
+        <Grid item md={7} lg={7}>
+          <div className="cart-total-container">
+            <div className="cart-total-heading">
+              <strong>Total:</strong>
+            </div>
+          </div>
+        </Grid>
+        <Grid item md={5} lg={5}>
+          <div className="cart-total-price-container">
+            <div className="cart-total-price">Rs 10039/-</div>
+          </div>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} style={{ margin: "10px" }}>
+        <Grid item md={7} lg={7}></Grid>
+        <Grid item md={5} lg={5}>
+          <div className="checkout-button-container">
+            <button className="button-black button-big">Checkout</button>
           </div>
         </Grid>
       </Grid>
